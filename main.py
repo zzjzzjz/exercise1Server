1e8c33402ec0d3570bc1d769eb4c6a3d0172e263
kaneki_my <2795188612@qq.com>
2022-09-18 20:45:30 +0800

    文件树+粘包问题2

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import threading
 import socket
 DEST_IP = "0.0.0.0"
 DEST_PORT = 9091
-BASE_PATH = "D://作业//网络攻击与防御"
+BASE_PATH = "D:\OneDrive - bestpigs\Videos\录屏"
 
 if __name__ == "__main__":
     th = threading.Thread(target=networkUtil.startFileDirectoryServer, args=(BASE_PATH, "0.0.0.0", 9090))  # 访问文件列表进程
